65e44d45ba202a70733e94348af3d8fa023a16d6
Chun <caochun@gmail.com>
2022-02-12 16:42:33 +0800

    update

---
+++ src/test/java/com/example/eventmachinecloud/EventmachineCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eventmachinecloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventmachineCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
